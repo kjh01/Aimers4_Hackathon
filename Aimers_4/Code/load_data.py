dce2ef5ec853e94672f15c5e37fd80b3246d1ebc
user <kjehy2001@daum.net>
2024-02-07 14:23:38 +0900

    commit.

---
+++ Aimers_4/Code/load_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+submission = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/submission.csv')
+print("[submission]")
+print(submission)
+
+train = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/train.csv')
+print("\n[train]")
+print(train)
+
+save = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/save.csv')
+print("\n[save]")
+print(save)
