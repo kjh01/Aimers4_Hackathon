d2f579e2d5acac787db4a97288fbfa8f33de7f2b
user <kjehy2001@daum.net>
2024-02-21 12:47:02 +0900

    commit.

--- Aimers_4/Code/load_data.py
+++ Aimers_4/Code/load_data.py
@@ -1,19 +1,19 @@
 import pandas as pd
-import numpy as np
 
-submission = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/submission.csv')
-print("[submission]")
-print(submission)
+# submission = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/submission.csv')
+# print("[submission]")
+# print(submission)
 
 train = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/train.csv')
 print("\n[train]")
 print(train.head(5))
 print(train.columns)
+print(train['business_unit'])
 
-save = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/save.csv')
-print("\n[save]")
-print(save)
-
-save2 = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/save2.csv')
-print("\n[save]")
-print(save2)
+# save = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/save.csv')
+# print("\n[save]")
+# print(save)
+#
+# save2 = pd.read_csv('C:/Users/user/PycharmProjects/pythonProject3/Aimers_4/Dataset/save2.csv')
+# print("\n[save]")
+# print(save2)
